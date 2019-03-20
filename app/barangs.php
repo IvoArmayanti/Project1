fc9a0911eb074cf5e45e84afdfb62b01f9fbdabb
IvoArmayanti <ivoarmayanti30@gmail.com>
2019-03-20 13:25:56 +0700

    file minggu ke 2

---
+++ app/barangs.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class barangs extends Model
+{
+    <?php
+namespace App; 
+use Illuminate\Database\Eloquent\Model;
+class barang extends Model
+{ 
+protected $table = 'barangs';
+} 
+}
+
+?>
